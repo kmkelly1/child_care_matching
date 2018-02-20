3c86b89959ad0a685bc181876b13194ed43cc547
Raghu Betina <raghu@firstdraft.com>
2018-02-20 01:27:29 +0000

    Generate parents resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :parents
+  # Routes for the Parent resource:
+  # READ
+  get "/parents", :controller => "parents", :action => "index"
+  get "/parents/:id", :controller => "parents", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
