f6efe172f8932a3590d055467acdedcd331babc7
Raghu Betina <raghu@firstdraft.com>
2018-02-20 01:28:36 +0000

    Whitelist columns in Reservation for ActiveAdmin

--- app/admin/reservation.rb
+++ app/admin/reservation.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Reservation do
 
+ permit_params :time, :childcare_hours, :available_hours
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
