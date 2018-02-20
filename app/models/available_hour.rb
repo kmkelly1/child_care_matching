cfeb5fa721cd18a036e0446bb4131d6842ea0181
Raghu Betina <raghu@firstdraft.com>
2018-02-20 01:27:57 +0000

    Direct association between available_hours and reservations

--- app/models/available_hour.rb
+++ app/models/available_hour.rb
@@ -1,6 +1,10 @@
 class AvailableHour < ApplicationRecord
   # Direct associations
 
+  has_one    :reservation,
+             :foreign_key => "available_hours",
+             :dependent => :destroy
+
   belongs_to :provider
 
   # Indirect associations
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,9 @@
 class Reservation < ApplicationRecord
   # Direct associations
 
+  belongs_to :available_hours,
+             :class_name => "AvailableHour"
+
   belongs_to :childcare_hours,
              :class_name => "ChildcareHour"
 
